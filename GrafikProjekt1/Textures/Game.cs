f08263138932f8a1e786ec91cbcb4dd2d7ac8507
Missenlover <94481329+Missenlover@users.noreply.github.com>
2024-02-23 12:45:24 +0100

    Main code

---
+++ GrafikProjekt1/Textures/Game.cs
@@ -0,0 +1,120 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ComputerGrafik
+{
+    public class Game : GameWindow
+    {
+        
+
+
+
+
+
+
+
+       
+        private List<GameObject> gameObjects = new List<GameObject>();
+
+        
+
+        public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
+        {
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            Matrix4 view = Matrix4.CreateTranslation(0.0f, 0, -3f);
+            Matrix4 projection = Matrix4.CreatePerspectiveFieldOfView(MathHelper.DegreesToRadians(60.0f),
+            (float)Size.X / (float)Size.Y, 0.3f, 1000.0f);
+            gameObjects.ForEach(x => x.Draw(view * projection));
+            SwapBuffers();
+
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
+            gameObjects.ForEach(x => x.Update(args));
+
+            
+            //Console.WriteLine((float)watch.Elapsed.TotalSeconds);
+            
+
+            
+
+            
+
+            
+
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            
+
+            
+
+            
+            Texture texture0 = new Texture("Textures/wall.jpg");
+            Texture texture1 = new Texture("Textures/AragonTexUdenBaggrund.png");
+    
+            
+
+            
+            Dictionary<string, object> uniforms = new Dictionary<string, object>();
+            uniforms.Add("texture0", texture0);
+            uniforms.Add("texture1", texture1);
+            Material mat = new Material("Shaders/shader.vert",
+            "Shaders/shader.frag", uniforms);
+            Renderer rend = new Renderer(mat, new QuadMesh());
+            Renderer rend2 = new Renderer(mat, new CubeMesh());
+            GameObject triangle = new GameObject(rend, this);
+            gameObjects.Add(triangle);
+            GameObject cube = new GameObject(rend2, this);
+            cube.transform.Position = new Vector3(1, 0, 0);
+            gameObjects.Add(cube);
+
+
+
+            GL.Enable(EnableCap.DepthTest);
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            
+        }
+
+        
+
+
+    }
+}
---
+++ GrafikProjekt1/Textures/GameObject.cs
@@ -0,0 +1,31 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerGrafik
+{
+    public class GameObject
+    {
+        public Transform transform;
+        public Renderer renderer;
+        public GameWindow gameWindow;
+        public GameObject(Renderer renderer, GameWindow gameWindow)
+        {
+            this.renderer = renderer;
+            this.gameWindow = gameWindow;
+            transform = new Transform();
+        }
+        public void Update(FrameEventArgs args)
+        {
+        }
+        public void Draw(Matrix4 vp)
+        {
+            renderer.Draw(transform.CalculateModel() * vp);
+        }
+    }
+}
